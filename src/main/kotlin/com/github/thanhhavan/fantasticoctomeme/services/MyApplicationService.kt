099367cf048ef4d12da95fa1d2fcc6cd415a9f6f
GitHub Action <action@github.com>
2022-08-02 09:13:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/thanhhavan/fantasticoctomeme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.thanhhavan.fantasticoctomeme.services
+
+import com.github.thanhhavan.fantasticoctomeme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
